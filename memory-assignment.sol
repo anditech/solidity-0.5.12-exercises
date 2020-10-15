b2a795b4850320531e20f7eb677637a02a3a288c
Andres <71329092+anditech@users.noreply.github.com>
2020-10-15 11:09:46 +0200

    Update memory-assignment.sol

--- memory-assignment.sol
+++ memory-assignment.sol
@@ -15,13 +15,13 @@ contract MemoryAndStorage {
     }
     // this function takes uint id & uint balance as arguments and updates balance
     function updateBalance(uint id, uint balance) public {
-        // we can update balance by changing it directly in the mapping 
+     // 1st solution: we can update balance by changing it directly in the mapping 
         users[id].balance = balance;
         
-      // When we have data location "memory"  the balance is not permanently stored and won't be updated.
-      // we need to change data location to "storage" to be able to update any changes in User struct 
-      //  User storage user = users[id];
-      //  user.balance = balance;
+     // 2nd solution: When we have data location "memory"  the balance is not permanently stored and won't be updated.
+     // we need to change data location to "storage" to be able to update any changes in User struct 
+     //  User storage user = users[id];
+     //  user.balance = balance;
       
     }
   
